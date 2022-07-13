142066a68ae6aac977ef93daf9937f56311b87e9
Stephen Davis <68449281+nativeit@users.noreply.github.com>
2022-07-13 06:37:30 -0400

    Create .env.dart

---
+++ lib/.env.dart
@@ -0,0 +1,14 @@
+class Config {
+  static const String API_SECRET = '';
+  static const String SENTRY_DNS = 'http://7fa95ee3dd3c4525ab4e44402f0a1209@sentry01.ntv.one/2';
+  static const String MICROSOFT_CLIENT_ID = '';
+
+  static const bool DEBUG_EVENTS = false;
+  static const bool DEBUG_REQUESTS = false;
+  static const bool DEMO_MODE = false;
+
+  static const String TEST_EMAIL = 'demo@invoiceninja.com';
+  static const String TEST_PASSWORD = 'Password0';
+  static const String TEST_URL = 'https://demo.invoiceninja.com';
+  static const String TEST_SECRET = '';
+}
